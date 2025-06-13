8d16c55acf0860d0f554d3f6df9864391557d9df
hs-office <hakimisaad.hs@gmail.com>
2025-06-13 20:06:57 +0800

    Update greet.test.js

--- greet.test.js
+++ greet.test.js
@@ -1,11 +1,11 @@
 test('greets Alice', () => {
-  expect(greet('Alice')).toBe('hello, Alice!');
+  expect(a('Alice')).toBe('hello, Alice!');
 });
 
 test('greets user with empty string', () => {
-  expect(greet('')).toBe('hello, !'); // maybe we want to handle this better later
+  expect(a('')).toBe('hello, !'); // maybe we want to handle this better later
 });
 
 test('greets undefined', () => {
-  expect(greet()).toBe('hello, undefined!');
+  expect(a()).toBe('hello, undefined!');
 });
